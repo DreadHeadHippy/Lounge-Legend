801eb3d46ac6810b2052fd2dff91e258494b1d3a
DreadHeadHippy <155098676+DreadHeadHippy@users.noreply.github.com>
2024-02-21 01:46:49 +0000

    Say.js
    I can reply to bot dm's as the bot

--- interactions/slash/misc/say.js
+++ interactions/slash/misc/say.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('say')
-    .setDescription('Make the bot say something in a specific channel')
+    .setDescription('Make the bot say something in a specific channel or to a user')
     .addStringOption(option =>
       option.setName('message')
         .setDescription('The message to be said')
@@ -12,20 +12,32 @@ module.exports = {
     .addChannelOption(option =>
       option.setName('channel')
         .setDescription('The channel where the message will be sent')
-        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option.setName('user')
+        .setDescription('The ID of the user to whom the message will be sent')
     ),
 
   async execute(interaction) {
     const message = interaction.options.getString('message');
     const channel = interaction.options.getChannel('channel');
+    const userId = interaction.options.getString('user');
 
     if (!interaction.member.permissions.has(BigInt(0x00000004))) {
       return interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
     }
 
     try {
-      await channel.send(message);
-      await interaction.reply(`Message sent in ${channel}: ${message}`);
+      if (channel) {
+        await channel.send(message);
+        await interaction.reply(`Message sent in ${channel}: ${message}`);
+      } else if (userId) {
+        const user = await interaction.client.users.fetch(userId);
+        await user.send(message);
+        await interaction.reply(`Message sent to ${user.tag}: ${message}`);
+      } else {
+        await interaction.reply({ content: 'Please specify either a channel or a user ID.', ephemeral: true });
+      }
     } catch (error) {
       console.error(error);
       await interaction.reply({ content: 'There was an error while trying to send the message.', ephemeral: true });
